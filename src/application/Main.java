4855a1fd53876bad89d5f274bc092c77d70445ea
Fiete Schmidt <f.schmidt.me@web.de>
2017-08-24 11:54:36 +0200

    nix

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,9 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
