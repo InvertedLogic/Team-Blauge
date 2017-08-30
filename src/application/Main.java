c68e146fdd250063773167b6a7a1673514e8a11a
TIRETECH\teichrib_a <teichrib.alex@gmail.com>
2017-08-30 11:41:08 +0200

    - Main.java zuächst so konfiguriert, dass die GUI schon mal gestartet werden kann. - Controllerklassen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,78 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
+	
+	BorderPane root = new BorderPane();
+	Stage primaryStage = new Stage();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+	        this.primaryStage = primaryStage;
+	        this.primaryStage.setTitle("BLAUGE Kanban");
+	        initRoot();
+	        //showGUI();
+	        showLogin();
+	        
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void initRoot() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("rootLayout.fxml"));
+            root = (BorderPane) loader.load();
+       
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public void showGUI() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("GUI.fxml"));
+            AnchorPane GUI = (AnchorPane) loader.load();
+
+            root.setCenter(GUI);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public void showLogin() {
+		 try {
+	            // Load person overview.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("loginScreen.fxml"));
+	            AnchorPane GUI = (AnchorPane) loader.load();
+
+	            root.setCenter(GUI);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+	
