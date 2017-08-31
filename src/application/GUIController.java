65a182e1e181b5ca8a6e3cc5a2a2034523e58a41
Fiete Schmidt <f.schmidt.me@web.de>
2017-08-31 11:56:39 +0200

    verschiedene dinge

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -1,8 +1,33 @@
 package application;
 
+import com.jfoenix.controls.JFXColorPicker;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ColorPicker;
+
 public class GUIController {
 	
 	private Main main;
+	
+	
+private JFXColorPicker colorPicker;		// https://github.com/jfoenixadmin/JFoenix/issues/408
+	
+	
+	public GUIController() {
+		this.colorPicker = new JFXColorPicker();
+        //this.colorPicker.editableProperty().bind(column.editableProperty());
+        //this.colorPicker.disableProperty().bind(column.editableProperty().not());
+        this.colorPicker.setOnShowing(event -> {
+            //nothing yet
+        });
+        this.colorPicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            
+        		//newValue is selected color
+                
+        });
+    
+		}
+	
 	
 	
 	public void setMainApp(Main main) {
