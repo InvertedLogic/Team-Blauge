f976e5a942d38a7fabcc23e41553f48f4ecb8d1b
SURFACE\Christopher <chris.rie@gmx.de>
2017-08-31 12:50:12 +0200

    Controller für LoginScreen

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,42 @@
 package application;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
+
+    @FXML
+    private JFXTextField textFieldLogInScreenUsername;
+
+    @FXML
+    private JFXPasswordField textFieldLogInScreenPassword;
+
+    @FXML
+    private JFXButton buttonLogInScreenLogIn;
+
+    @FXML
+    private JFXButton buttonLogInScreenSettings;
+
+    @FXML
+    private JFXButton buttonLogInScreenInformation;
+
+    @FXML
+    void buttonLogInScreenInformationPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonLogInScreenLogInPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonLogInScreenSettingsPressed(ActionEvent event) {
+
+    }
 
 }
