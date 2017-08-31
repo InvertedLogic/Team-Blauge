1d6ea6987cb7eb1c75857afc35926bd9b558527e
Lars Raschke <vrock@gmx.de>
2017-08-31 13:06:22 +0200

    Update : User

--- src/controlling/KommController.java
+++ src/controlling/KommController.java
@@ -16,17 +16,17 @@ public interface KommController extends Remote {
 	String DEFAULT_RMI_OBJECT_NAME = "Blauge";
 
 	/* Ausbaustufe I */
-	public boolean projektCreate(User u, String bezeichnung) throws RemoteException;
-	public boolean projectFinalize() throws RemoteException;
-	public boolean taskCreate(String name, String kommentar) throws RemoteException;
-	public boolean taskEdit() throws RemoteException;
-	public boolean taskMoveFwd(int id) throws RemoteException;
-	public boolean taskMoveRwd(int id) throws RemoteException;
+	boolean projektCreate(User u, String bezeichnung) throws RemoteException;
+	boolean projectFinalize() throws RemoteException;
+	boolean taskCreate(String name, String kommentar) throws RemoteException;
+	boolean taskEdit() throws RemoteException;
+	boolean taskMoveFwd(int id) throws RemoteException;
+	boolean taskMoveRwd(int id) throws RemoteException;
 
 	/* Ausbaustufe II */
-	public boolean addUser(User u, Projekt p) throws RemoteException;
+	boolean addUser(User u, Projekt p) throws RemoteException;
 
 	/* Ausbaustufe IV */
-	public boolean addStatus(String name) throws RemoteException;
-	public boolean changeStatusName(String name, int id) throws RemoteException;
+	boolean addStatus(String name) throws RemoteException;
+	boolean changeStatusName(String name, int id) throws RemoteException;
 }
