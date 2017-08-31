69a006154400a5445fe094fe5f09e512e01adc1d
uidp3323 <uidp3323@FRL5D7XG.auto.contiwan.com>
2017-08-31 12:12:15 +0200

    Implemented Interface

--- src/testCom/Server.java
+++ src/testCom/Server.java
@@ -7,9 +7,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import model.Projekt;
+import model.User;
+
 public class Server extends UnicastRemoteObject implements KommController{
+
+	protected Server() throws RemoteException{};
 
 	static ComProtocol protocol;
 	public static int clientcounter = 0;
@@ -55,11 +61,57 @@ public class Server extends UnicastRemoteObject implements KommController{
 		}
 	}
 
-	public static int functionA(int a, int b) {
-		return (a * b);
+	@Override
+	public boolean projektCreate(User u, String bezeichnung){
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	public static int functionB(int a, int b) {
-		return (a * b * a * b);
+	@Override
+	public boolean projectFinalize(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean taskCreate(String name, String Kommentar, User u){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean taskEdit(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean taskMoveFwd(int id){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean taskMoveRwd(int id){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addUser(User u, Projekt p){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addStatus(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean changeStatusName(){
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
