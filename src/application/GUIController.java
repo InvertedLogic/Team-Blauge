2630070b4a33cd7ec54b6cab6ded55953f473eaf
SURFACE\Christopher <chris.rie@gmx.de>
2017-08-31 12:44:30 +0200

    Gui update und Variablen und Funktionen für Gui Controller

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -1,36 +1,184 @@
 package application;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXColorPicker;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXMasonryPane;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 
 public class GUIController {
-	
+	@FXML
+    private Label labelProjectname;
+
+    @FXML
+    private Label labelProjectinformation;
+
+    @FXML
+    private Label labelUser;
+
+    @FXML
+    private MenuButton menuButtonFilter;
+
+    @FXML
+    private Label labelTaskname;
+
+    @FXML
+    private Label labelAuthor;
+
+    @FXML
+    private Label labelStatus;
+
+    @FXML
+    private Label labelColor;
+
+    @FXML
+    private Label labelDescription;
+
+    @FXML
+    private Label labelTags;
+
+    @FXML
+    private Label labelActualAuthor;
+
+    @FXML
+    private Label labelActualStatus;
+
+    @FXML
+    private JFXTextField textFieldTaskname;
+
+    @FXML
+    private JFXColorPicker ColorPicker;
+
+    @FXML
+    private JFXTextArea textAreaDescription;
+
+    @FXML
+    private JFXListView<?> listViewTags;
+
+    @FXML
+    private Label labelComments;
+
+    @FXML
+    private JFXListView<?> listViewComments;
+
+    @FXML
+    private JFXTextField textFieldTags;
+
+    @FXML
+    private JFXButton buttonAddTag;
+
+    @FXML
+    private JFXTextField textFieldComments;
+
+    @FXML
+    private JFXButton buttonAddComment;
+
+    @FXML
+    private JFXButton buttonProjectselection;
+
+    @FXML
+    private JFXButton buttonLogOut;
+
+    @FXML
+    private JFXButton buttonProceed;
+
+    @FXML
+    private JFXButton buttonReturn;
+
+    @FXML
+    private JFXButton buttonInformation;
+
+    @FXML
+    private JFXButton buttonTags;
+
+    @FXML
+    private JFXButton buttonComments;
+
+    @FXML
+    private Label labelToDo;
+
+    @FXML
+    private Label labelDoing;
+
+    @FXML
+    private Label labelFinished;
+
+    @FXML
+    private JFXMasonryPane mansoryPaneToDo;
+
+    @FXML
+    private JFXMasonryPane mansoryPaneDoing;
+
+    @FXML
+    private JFXMasonryPane mansoryPaneFinished;
+
 	private Main main;
-	
-	
-private JFXColorPicker colorPicker;		// https://github.com/jfoenixadmin/JFoenix/issues/408
-	
-	
+
+	private JFXColorPicker colorPicker; // https://github.com/jfoenixadmin/JFoenix/issues/408
+
 	public GUIController() {
 		this.colorPicker = new JFXColorPicker();
-        //this.colorPicker.editableProperty().bind(column.editableProperty());
-        //this.colorPicker.disableProperty().bind(column.editableProperty().not());
-        this.colorPicker.setOnShowing(event -> {
-            //nothing yet
-        });
-        this.colorPicker.valueProperty().addListener((observable, oldValue, newValue) -> {
-            
-        		//newValue is selected color
-                
-        });
-    
-		}
-	
-	
-	
+		// this.colorPicker.editableProperty().bind(column.editableProperty());
+		// this.colorPicker.disableProperty().bind(column.editableProperty().not());
+		this.colorPicker.setOnShowing(event -> {
+			// nothing yet
+		});
+		this.colorPicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+
+			// newValue is selected color
+
+		});
+
+	}
+
 	public void setMainApp(Main main) {
 		this.main = main;
 	}
+	
+	@FXML
+    void ColorPickerSelectionChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonCommentsPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonInformationPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonLogOutPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonProceedPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonProjectselectionPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonReturnPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonTagsPressed(ActionEvent event) {
+
+    }
 }
