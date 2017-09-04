fb49164d50d6f8e294fb56651b24c9b60a73795d
Fiete Schmidt <f.schmidt.me@web.de>
2017-09-04 14:26:17 +0200

    tried compose and save button to change when clicked

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -1,6 +1,11 @@
 package application;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXColorPicker;
@@ -22,6 +27,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
 
 public class GUIController {
 	@FXML
@@ -226,8 +232,13 @@ public class GUIController {
         	saveEnteredTaskname(textFieldTaskname.getText());
     		/*
     		 * TODO:
-    		 * ändere das icon des buttons wieder zu 'Stift'
+    		 * ändere das icon des buttons zu 'Compose' funktioniert noch nicht
     		 */
+        	File f = new File("/src/resources/Icons/compose.png");
+        	Image img = new Image(f.toURI().toString(), true);
+        	buttonEditTaskNameIcon.setImage(img);
+        	System.out.println("Button zu compose.png geaendert");
+        		
     	}
     	else {
     		
@@ -242,18 +253,30 @@ public class GUIController {
 	                	
 	                    /*
 	                     * TODO:
-	                     * ändere Buttonicon wieder zu stift
+	                     * ändere Buttonicon wieder zu stift funktioniert noch nicht
 	                     * wenn >TAB< gedrueckt waehrend im Textfield soll fokus auf Button gelegt werden
 	                     */ 
-	                	//File file = new File("/resources/Icons/save.png");
-	                	Image img = new Image("./src/main/resources/Icons/save.png",true);
+	                	File f = new File("/src/resources/Icons/compose.png");
+	                	Image img = new Image("file:\\src\\resources\\Icons\\compose.png");
 	                	buttonEditTaskNameIcon.setImage(img);
-	                	//buttonEditTaskNameIcon.getImage();
+	                	System.out.println("Button zu Compose.png geaendert");
+	                	
+
+	                	System.out.println(f.getAbsolutePath());
+	                	System.out.println(f.toURI().toString());
+	                	
+
+	                	
 	                }
 	            } 
 	        });
     	
     		textFieldTaskname.editableProperty().set(true);
+    		
+    		File f = new File("file:\\src\\resources\\Icons\\save.png");
+        	Image img = new Image(f.toURI().toString(), true);
+        	buttonEditTaskNameIcon.setImage(img);
+        	System.out.println("Button zu save.png geaendert");
     		
     		/*
     		 * TODO:
